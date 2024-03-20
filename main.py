09b4f49c1b8f938c98c4ceae78b8062a84c48d2b
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-20 21:43:32 +0300

    User Interface designed and composed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Calibri"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -16,3 +17,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodore")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(101, 112, image=tomato_img)
+# ---Creating the text
+canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+
+
+
+canvas.pack()
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
