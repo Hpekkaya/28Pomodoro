353de2991b7896a5d7facf29da3eec83e28d6304
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-20 22:11:39 +0300

    User Interface completed

--- main.py
+++ main.py
@@ -21,17 +21,30 @@ window = Tk()
 window.title("Pomodore")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
+# ---Creating the Canvas
 canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(101, 112, image=tomato_img)
 # ---Creating the text
 canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+# ---Adding the other Widgets
+label_timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, ))
+label_timer.grid(column=1, row=0)
+
+start_button = Button(text="Start", width=7, bg=YELLOW, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", width=7, bg=YELLOW, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔", fg=GREEN, bg=YELLOW, font=50)
+check_mark.grid(column=1, row=3)
 
 
 
 
-canvas.pack()
+
 
 
 
